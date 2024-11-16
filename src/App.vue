1d03094a9a56b31ba8b8a71edc08b2429932ef3d
Ziery-Dev <zieryDev@gmail.com>
2024-11-16 16:29:19 -0300

    Trabalhando com componentes

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+ <!--script-->
+ <<script setup> 
+  import Ex1 from './componentes/Exemplo1-interpolacao.vue'
+ </script>
+ 
+ <<template>
+  <Ex1></Ex1>
+ </template>
---
+++ src/componentes/Exemplo1-interpolacao.vue
@@ -0,0 +1,13 @@
+ <!--script-->
+<script setup>
+  let nome = "Ziery";
+  let idade = 25;
+  let num = 2;
+  let num2 = 50;
+</script>
+
+ <!--html-->
+<template>
+  <p>{{"Olá " + nome}}</p>
+  <p>{{num+num2}}</p>
+</template>
