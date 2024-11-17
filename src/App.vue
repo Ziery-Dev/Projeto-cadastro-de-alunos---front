6839ce12f2579ee4eef3c40f4cc62ee72e88f894
Ziery-Dev <zieryDev@gmail.com>
2024-11-17 12:38:44 -0300

    Eventos de teclado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
  <!--script-->
  <<script setup> 
-  import Ex from './Componentes/eventoClique.vue'
+  import Ex from './Componentes/eventosTeclado.vue'
  </script>
 
 
---
+++ src/componentes/eventosTeclado.vue
@@ -0,0 +1,24 @@
+
+ <!--script-->
+<script setup>
+
+    function mensagem (){
+        alert("teclou");
+    }
+
+    let validacao = true;
+</script>
+
+<!--css-->
+<style >
+  
+    
+
+</style>
+
+ <!--html-->
+<template>
+<h2>Digite alguma coisa:</h2>
+   <input type="text" @keyup.enter="mensagem"> <!--Ao clicar no botão especificado, chama a função que exibe um alerta com a mensagem (essa função pode ser outra coisa) -->
+
+</template>
